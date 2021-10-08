44da2f5ee26ca32ecf041498f0b8d5ed1f66787d
Cory Loeffelman <frontenddevemail@gmail.com>
2021-10-08 12:47:40 -0500

    Reorg for transfer to Waff Apps Org.

--- tickerr-client/components/app/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface AppProps {}
-
-export const App: React.FC<AppProps> = (props: AppProps) => {
-  return (
-    <div id="tickerr-app" />
-  )
-}
--- tickerr-client/firebase.ts
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-
-import { tickerrProductionAppConfig } from "../config/firebaseConfig";
-
-const getConfig = (): any => tickerrProductionAppConfig;
-
-firebase.initializeApp(getConfig());
